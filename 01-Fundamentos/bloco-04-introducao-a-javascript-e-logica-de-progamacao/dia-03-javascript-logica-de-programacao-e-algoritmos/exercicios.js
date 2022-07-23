bece899405e662e5b010ef924882c29f18c548da
Gabriel Dota <gabrieldotafujita1234@hotmail.com>
2022-07-23 17:06:41 -0300

    continuo depois

--- 01-Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ 01-Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -4,6 +4,7 @@ fatorial = 10
 for (let i = 1; i <= fatorial; i += 1) {
   multiplica = resultado * i
   resultado = multiplica
+  console.log(resultado)
 }
 
 //exercicio 2
@@ -16,7 +17,7 @@ let inverte = ''
 
 
 //exercicio 3
-let array = ['java', 'javascript', 'python', 'html', 'ablublubleeee', 'css'];
+let array = ['java', 'javascript', 'python', 'html', 'css'];
 /* criar variavel com a maior string
 criar loop for
 criar variaveeis para armazenar as strings
@@ -38,5 +39,54 @@ for (i = 0; i < array.length; i += 1) {
         maiorString = string1;
       }
     }
+  }
+} console.log("A maior string é " + maiorString)
+
+let menorString = ''
+for (i = 0; i < array.length; i += 1) {
+  string1 = array[i]
+  for (j = 0; j < i; j += 1) {
+    string2 = array[j]
+    if(string1.length < string2.length
+      ){
+      if(string1.length < maiorString.length){
+        menorString = string1;
+      }
+    }
+  }
+} console.log("A menos string é " + menorString)
+
+// exercicio 4
+/*criar variavel pra saber o limite = qntsPrimos
+criar um array resultado para saber quais são os numeros primos
+abrir loop for com limite na variavel qntsPrimos
+criar variavel para armazenar quantos divisores
+fazer loop dentro de loop
+criar a variavel divisor
+cada vez que o numero for divisor adicionar a contagem
+se a contagem for 2, ele é um numero primo
+adiciona esse numero no array resultado
+compara pra saber qual é o maior numero primo entre eles*/
+
+let qntsPrimos = 50;
+let primos = [];
+let maiorPrimo = 
+
+for (let index = 2; index <= qntsPrimos; index += 1) {
+  let qntsDivisores = 0
+  for (let divisor = 1; divisor >= index; divisor += 1) {
+    if(index % divisor === 0){
+      qntsDivisores += 1;
+    }
+    if(qntsDivisores === 2){
+      array.push(index);
+    }
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  for (let j = 0; j >= i; j++) {
+    const element = array[j];
+    
   }
 }
